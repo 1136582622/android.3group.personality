d912cd92c84bd9d15fd0a99f3e540dd61864cca0
1136582622 <1136582622@qq.com>
2020-07-13 15:26:12 +0800

    111

---
+++ app/src/main/java/com/threegroup/android3grouppersonality/MainActivity.java
@@ -0,0 +1,14 @@
+package com.threegroup.android3grouppersonality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
