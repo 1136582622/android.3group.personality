fd525dcef77596e9173995191df94232011e0d43
1136582622 <1136582622@qq.com>
2020-07-20 11:27:10 +0800

    2020.7.18  11：38

---
+++ app/src/main/java/com/threegroup/android3grouppersonality/pojo/Comment.java
@@ -0,0 +1,61 @@
+package com.threegroup.android3grouppersonality.pojo;
+
+import java.io.Serializable;
+
+public class Comment implements Serializable {
+    private int id;  //id
+    private String content; //内容
+    private String time; //发送时间
+    private Blog blog; //博客实体
+    private User user; //用户实体
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Blog getBlog() {
+        return blog;
+    }
+
+    public void setBlog(Blog blog) {
+        this.blog = blog;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", time='" + time + '\'' +
+                ", blog=" + blog +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/threegroup/android3grouppersonality/pojo/User.java
@@ -0,0 +1,82 @@
+package com.threegroup.android3grouppersonality.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    //Alt+insert，getter and setter,toString
+    private String userid; //用户Id string,和数据库类型一致，不是整形
+    private String name;  //姓名
+    private String sex;   //性别
+    private String phone;  /**电话，和id一致用电话号码登录**/
+    private String password; //密码
+    private String photo;  //头像
+    private String introduction;//简介
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid='" + userid + '\'' +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                ", introduction='" + introduction + '\'' +
+                ", photo='" + photo + '\'' +
+                '}';
+    }
+}
