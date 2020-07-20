98a705a33b0c2b46818ccc1fcdf8a7c9054865ae
1136582622 <1136582622@qq.com>
2020-07-20 15:10:25 +0800

    2020-7-20 12：00 李世平

--- app/src/main/java/com/threegroup/android3grouppersonality/utils/Tool.java
+++ app/src/main/java/com/threegroup/android3grouppersonality/utils/Tool.java
@@ -3,7 +3,8 @@ package com.threegroup.android3grouppersonality.utils;
 * ip工具类
 * */
 public class Tool {
-    public static final String url="http://192.168.43.51:8080/com.3group.personality/"; //李世平
+    public static final String url="http://192.168.43.51:8080/com.3group.personality/"; //李世平手机
+    /*public static final String url="http://192.168.0.114:8080/com.3group.personality/"; *///李世平宿舍
 
   /*  public static final String url="http://192.168.43.51:8080/com.3group.personality/"; //张有明
 
